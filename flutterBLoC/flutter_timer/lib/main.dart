9055f49cb92067692cc2c57b39768e9c356f9ee3
mikaflatah <mikaflatah@gmail.com>
2024-05-23 15:58:55 +0530

    timer app

---
+++ flutterBLoC/flutter_timer/lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_timer/app.dart';
+
+void main() => runApp(const App());
+
---
+++ flutterBLoC/flutter_timer/lib/timer/timer.dart
@@ -0,0 +1,2 @@
+export 'timer_bloc.dart';
+export 'package:flutter_timer/view/timer_page.dart';
